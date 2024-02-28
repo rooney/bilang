e8272f99bde1ef97c505d5467b9994b01bfaaf4a
Ron Panduwana <panduwana@gmail.com>
2024-02-28 12:27:26 +0700

    Initial expander

--- expander.rkt
+++ expander.rkt
@@ -7,32 +7,27 @@
 (define-for-syntax (debug x)
   (println (syntax->datum x)) x)
 
-(define string string-append)
-(define int    identity)
-(define dec    identity)
-(define name   identity)
-(define expres identity)
-(provide string int dec name expres)
+(define-macro string #'string-append)
+(define-macro int    #'identity)
+(define-macro dec    #'identity)
+(define-macro name   #'identity)
+(define-macro comma  #'identity)
+(define-macro parens #'identity)
+(define-macro expres #'identity)
+(provide string int dec name comma parens expres)
 
 (define (bilang-let x) (lambda (f) (f x)))
 (provide (rename-out [bilang-let let]))
 
-(define-macro-cases comma1
-  [(comma1 (name N) X)    (debug #'(app (atom N) X))]
-  [(comma1 F X)           (debug #'(app F X))])
+(define-macro-cases apply3
+  [(apply3 (expl2 (apply1 F (parens (name VARNAME))) (rad (anion)) VALUE) BODY) #'((F VALUE) (lambda (VARNAME) BODY))])
 
 (define-macro-cases app
-  [(app (atom A RESTRICT ...) (braces BODY) ARG ...) (debug #'(app (lambda (A) BODY) ARG ...))]
-  [(app (atom A RESTRICT ...) X ARG ...)             (debug #'(app (atom A RESTRICT ... X) ARG ...))]
-  [(app (comma1 (name N) X) ARG ...)                 (debug #'(app (atom N) X ARG ...))]
-  [(app (parens (name N)) ARG ...)                   (debug #'(app (atom N) ARG ...))]
-  [(app (parens X) ARG ...)                          (debug #'(app X ARG ...))]
-  [(app (apply1 F X) ARG ...)                        (debug #'(app F X ARG ...))]
-  [(app (comma1 F X) ARG ...)                        (debug #'(app F X ARG ...))]
-  [(app F X Y ...)                                   (debug #'(app (F X) Y ...))]
-  [(app X)                                           (debug #'X)])
+  [(app (parens (comma (name N))) (braces BODY)) (debug #'(lambda (N) BODY))]
+  [(app (parens        (name N))  (braces BODY)) (debug #'(lambda (N) BODY))]
+  [(app F X)                                     (debug #'(F X))])
 
 (define-macro apply1 #'app)
+(define-macro comma1 #'app)
 
-(provide apply1 comma1)
-
+(provide apply3 apply1 comma1)
