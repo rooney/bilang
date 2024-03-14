daa5e42e0508ffc4b3d64801a6a12060f6e2e783
Ron Panduwana <panduwana@gmail.com>
2024-03-14 11:08:04 +0700

    perf: faster parser

--- parser.rkt
+++ parser.rkt
@@ -3,7 +3,7 @@
 expres : newline? expr3
 @expr3 : apply3
        | exprB
-@exprB : break|breakO|break1|breakL|break2
+@exprB : breakI|breakL|break2
        | expr2
 @expr2 : apply2|comma2|rad2
        | exprL
@@ -21,54 +21,56 @@ expres : newline? expr3
        | dot
        | it
        | e
-radL   :                                        rad kwargs
-       |                        exprJ  /SPACE   rad kwargs?
-       |                       (exprJ  /SPACE)? rad kwargs?  /SPACE (kvL|exprL)
-rad2   :  (@break|breakO|break1)       /SPACE   rad kwargs? (/SPACE (kvL|exprL))?
-       | ((@break|breakO|break1|exprJ) /SPACE)? rad kwargs? (/SPACE (kv2|apply2|comma2|rad2)|dent)
 
-commaO :  @comma        (dot|op) (dot|op|string|group)*
-breakO :  @break        (dot|op) (dot|op|string|group)*
-comma1 : (@comma|commaO) kwargs? /SPACE (kv|expr1)          |           exprI /SPACE /COMMA (kv|expr0)
-break1 : (@break|breakO) kwargs? /SPACE (kv|expr1)          | (break1|breakO) /SPACE /COMMA (kv|expr0)
-commaL : (@comma|commaO) kwargs? /SPACE (kvL|applyL)        |           exprI /SPACE /COMMA  kvL
-breakL : (@break|breakO) kwargs? /SPACE (kvL|applyL)        | (break1|breakO) /SPACE /COMMA  kvL | exprB newline kvL
-comma2 : (@comma|commaO) kwargs? (/SPACE (kv2|apply2)|dent) |           exprI /SPACE /COMMA  kv2
-break2 : (@break|breakO) kwargs? (/SPACE (kv2|apply2)|dent) | (break1|breakO) /SPACE /COMMA  kv2 | exprB newline kv2
-comma  :  exprI /SPACE? /COMMA
-break  :  exprB newline /COMMA                              | (break1|breakO) /SPACE? /COMMA
+radL   : rad kwargs
+       | Js rk
+       | Js? rk sLx
+rad2   : bIs rk sLx?
+       | (bIs|Js)? rk (/SPACE (kv2|x2)|dent)
+
+commaO : @comma dodo
+breakO : @break dodo
+comma1 : ck s1 | exprI sc0
+break1 : bk s1 | break1O sc0
+commaL : ck sL | exprI scL
+breakL : bk sL | break1O scL | Bn kvL
+comma2 : ck s2 | exprI sc2
+break2 : bk s2 | break1O sc2 | Bn kv2
+comma  : exprI sqc
+break  : exprB nc | break1O sqc
 apply3 : (exprB|rad) newline expr3
-apply2 :  exprO kwargs? (/SPACE (kv2|apply2)|dent)
-applyL :  exprO kwargs? /SPACE (kvL|applyL)
-apply1 :  exprO kwargs? /SPACE (kv|expr1)
-apply0 :  expr0 (dot|op|string|group)+
-       |  ion (op|e)
-       |  op e
-@e     :  slot
-       |  unit
-       |  int|dec|id|string|group
-unit   : (int|dec) id
-int    :  INTEGER
-dec    :  DECIMAL
-id     : (DOLLAR|DASH)? IDENTIFIER QUESTION? PRIME*
-op     : (DOLLAR|DASH|SLASH|PLUS|OP|QUESTION) PRIME*
-       |  LPAREN RPAREN
-       |  LBRACE RBRACE
-       |  DOT DOT
+apply2 : Ok s2
+applyL : Ok sL
+apply1 : Ok s1
+apply0 : expr0 dogs+
+       | ion (op|e)
+       | op e
+
+@e     : slot
+       | unit
+       | num|id|gs
+unit   : num id
+int    : INTEGER
+dec    : DECIMAL
+id     : prefix? IDENTIFIER QUESTION? PRIME*
+op     : (prefix|SLASH|PLUS|OP|QUESTION) PRIME*
+       | LPAREN RPAREN
+       | LBRACE RBRACE
+       | DOT DOT
 dot    : /DOT SLASH? id
        | /DOT /LPAREN @op /RPAREN
 prop   : @dot /COLON exprO
-kv     : @key /COLON exprO
-kvL    : @key /COLON /SPACE exprL
-kv2    : @key /COLON (/SPACE (apply2|comma2|rad2)|dent)?
-ion    :      /COLON
+kv     : k exprO
+kvL    : k sXL
+kv2    : k (/SPACE x2|dent)?
+ion    : /COLON
 rad    : @op | COLON
 key    : @op | @id
 it     : /IT
 string : /QUOTE str* /UNQUOTE
        | /GRAVE str+
-       | /QUOTE blockquo /NEWLINE NEWLINE* /UNQUOTE
-       | /GRAVE blockquo /NEWLINE NEWLINE*
+	   | /QUOTE bnn /UNQUOTE
+       | /GRAVE bnn
 @str   : STRING|blockquo|NEWLINE
        | @inter|interp|interpd
        | LPAREN str RPAREN
@@ -86,13 +88,45 @@ interpd  : /INTERP dent
 @kwargs  : (/SPACE kv)+
 @subexpr : (/SPACE? apply2|dent) newline
          |  /SPACE? exprL /SPACE?
-@array   :  /SPACE  exprL                     (newline /COMMA  /SPACE exprL)* /SPACE
-         |          expr0 (/SPACE expr0)*     (newline  expr0 (/SPACE expr0)*)*
-         |  indent /COMMA (/SPACE exprL|dent) (newline /COMMA (/SPACE exprL|dent))* /DEDENT newline
-@dent    :  indent @expres /DEDENT
-blockquo :  indent str+ /DEDENT
-@indent  :  newline /INDENT
+@array   :  /SPACE  exprL (nc sXL)* /SPACE
+         |          expr0 s0* (newline expr0 s0*)*
+         | indent /COMMA sXd (nc sXd)* /DEDENT newline
+@dent    : indent @expres /DEDENT
+blockquo : indent str+ /DEDENT
+@indent  : newline /INDENT
 @newline : /NEWLINE+
+
+@rk   : rad kwargs?
+@Ok   : exprO kwargs?
+@bk   : (@break|breakO) kwargs?
+@ck   : (@comma|commaO) kwargs?
+@nc   : newline /COMMA
+@sqc  : /SPACE? /COMMA
+@sc   : /SPACE /COMMA
+@s0   : /SPACE expr0
+@s1   : /SPACE (kv|expr1)
+@s2   : /SPACE (kv2|apply2)|dent
+@sL   : /SPACE (kvL|applyL)
+@sLx  : /SPACE (kvL|exprL)
+@sXL  : /SPACE exprL
+@sXd  : sXL|dent
+@scL  : sc kvL
+@sc2  : sc kv2
+@sc0  : sc (kv|expr0)
+@Js   : exprJ /SPACE
+@bIs  : breakI /SPACE
+@k    : @key /COLON
+@bnn  : blockquo /NEWLINE NEWLINE*
+@num  : int|dec
+@gs   : group|string
+@dop  : dot|op
+@dogs : dop|gs
+@dodo : dop dogs*
+@Bn   : exprB newline
+@x2   : apply2|comma2|rad2
+@breakI  : @break|break1O
+@break1O : break1|breakO
+@prefix  : DOLLAR|DASH
 
 error : /UNKNOWN-TOKEN
       | /WANT-TABS
